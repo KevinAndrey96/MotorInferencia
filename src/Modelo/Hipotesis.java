90daff11b8e5b347f4dfa1d3b6f114ae5cb85194
Ortiz PC <Ortiz PC@DESKTOP-8V6TRTS>
2017-11-02 21:52:22 -0500

    danny una pequeña modificacion que coloque

--- src/Modelo/Hipotesis.java
+++ src/Modelo/Hipotesis.java
@@ -7,8 +7,76 @@ package Modelo;
 
 /**
  *
- * @author Kevin
+ * @author Ortiz PC
  */
 public class Hipotesis {
+    
+    
+   public String Atributo,valor,descripcion;
+   public int Regla_No;
+
+    public Hipotesis(String Atributo, String valor, String descripcion, int Regla_No) {
+        this.Atributo = Atributo;
+        this.valor = valor;
+        this.descripcion = descripcion;
+        this.Regla_No = Regla_No;
+    }
+
+    public Hipotesis() {
+    
+        
+        this.Atributo = "";
+        this.valor = "";
+        this.descripcion = "";
+        this.Regla_No = 0;
+    }
+
+    public String getAtributo() {
+        return Atributo;
+    }
+
+    public void setAtributo(String Atributo) {
+        this.Atributo = Atributo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getRegla_No() {
+        return Regla_No;
+    }
+
+    public void setRegla_No(int Regla_No) {
+        this.Regla_No = Regla_No;
+    }
+
+    @Override
+    public String toString() {
+        return "-----Hipotesis---" 
+                + "\nAtributo=" + Atributo 
+                + "\nValor=" + valor 
+                + "\nDescripcion=" + descripcion 
+                + "\nRegla_No=" + Regla_No ;
+    }
+   
+    
+   
+   
+   
+            
+     
     
 }
